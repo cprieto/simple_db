bde1597e4b510c7da99413fe94e23bc67d0c3628
Cristian Prieto <me@cprieto.com>
2022-06-12 18:30:04 +0200

    Fix weird error :|

--- src/main.c
+++ src/main.c
@@ -11,8 +11,8 @@ typedef enum {
 
 MetaCommandResult do_meta_command(InputBuffer* input_buffer, Table* table) {
     if (strncmp(input_buffer->buffer, ".exit", input_buffer->buffer_length) == 0) {
-        close_input_buffer(input_buffer);
-        free_table(table);
+        // close_input_buffer(input_buffer);
+        // free_table(table);
 
         exit(EXIT_SUCCESS);
     } else {
